23282c38b39af48dc40ff2eb5f87268feb9b4289
Diógenes Silva <diogenes.silva88@gmail.com>
2025-06-02 17:44:02 -0300

    Commit inicial do projeto DailyBrief.AI

---
+++ Entities/Nota.cs
@@ -0,0 +1,9 @@
+﻿namespace DailyBrief.AI.Entities;
+
+public class Nota
+{
+    public Guid Id { get; set; }
+    public string Titulo { get; set; }
+    public string Conteudo { get; set; }
+    public DateTime DataCriacao { get; set; }
+}
